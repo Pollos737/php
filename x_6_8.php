e0a4ee6c3b6b61d694f3d99839d76bd858a684eb
Wilmer <Wilmer@iMac-2.local>
2016-02-26 16:57:20 -0800

    contiuned 6.8

--- x_6_8.php
+++ x_6_8.php
@@ -5,6 +5,21 @@
     <title>6.8 Array Basics</title>
 </head>
 <body>
+    <ul>
+    <?php
+        $items = array();
 
+        for($i = 1; $i <= 5; $i++){
+            $user_input_html = 'item'.$i;
+            $userInput = $_POST[$user_input_html];
+
+            array_push($items, $userInput);
+
+            $array_number = $i - 1;
+
+            print "<li>$items[$array_number]</li>";
+        }
+    ?>
+    </ul
 </body>
 </html>
