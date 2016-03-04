40fbff807bd4cdd24d5ffdc2fb0d2b0755e2413c
Wilmer <Wilmer@Gohan.local>
2016-03-04 00:10:12 -0800

    started 6.12

---
+++ x_6_12.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Recursive Programming 6.12</title>
+    <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+<main>
+
+</main>
+</body>
+</html>
